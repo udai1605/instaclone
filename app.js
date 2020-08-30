5106909f61390d0b9d88106d32a5dc42a6014e28
udai1605 <rudaikiran@gmail.com>
2020-08-30 23:17:55 +0530

    added authentication

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express= require('express');
+const app = express();
+const mongoose=require('mongoose');
+require("./models/user.js");
+
+app.use(express.json());
+app.use(require("./routes/auth.js"));
+
+
+mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to Mongo Server');
+})
+mongoose.connection.on('error',(err)=>{
+    console.log('Error Connecting To Database');
+})
+
+
+
+
+
+
+app.get('/',function(req,res){
+    res.send("hello world");
+})
+
+app.listen(3000,function(){
+    console.log("server started"); 
+})
---
+++ routes/auth.js
@@ -0,0 +1,68 @@
+const express= require('express');
+const router=express.Router();
+const mongoose= require('mongoose');
+const User = mongoose.model("User");
+const bcrypt = require('bcryptjs');
+
+router.get('/',(req, res)=>{
+    res.send("hello")
+})
+
+router.post('/signup',(req, res)=>{
+    const {name,email,password} = req.body
+    if(!email|| !password|| !name){
+        return res.status(422).json({error:"please add all fields"}) //to make the server understand that its a error and dont process it 
+    }                                                          // 422 is an unprocessable entity
+    User.findOne({email:email})
+    .then((savedUser) => {
+        if(savedUser){
+            return res.status(422).json({error:"User already exists"})
+        }
+        bcrypt.hash(password,12)
+        .then(hashedpassword =>{
+            const user= new User({
+                email,
+                password:hashedpassword,
+                name
+            })
+            user.save()
+            .then(user => {
+                res.json({message:"Registered Succesfully"})
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+        })
+        
+})
+
+router.post('/signin', (req, res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        res.status(422).json({error:"Provide The Required Fields"})
+    }
+    User.findOne({email: email})
+    .then(savedUser=>{
+        if(!savedUser){
+            res.status(422).json({error:"invalid details"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then(match => {
+            if(match){
+                res.json({message:"Succesfully Signed In"})
+            }else{
+                return res.status(422).json({error:"Invalid details"})
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    })
+})
+
+module.exports =router
